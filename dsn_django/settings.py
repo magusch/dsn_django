92e77f59e20296400105df497f4416b030e115a6
magusch <magusch@gmail.com>
2022-10-27 18:53:15 +0300

    Docker 2.1 – whitenoise, new requirements.txt

--- dsn_django/settings.py
+++ dsn_django/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -111,7 +112,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-#STATIC_ROOT = BASE_DIR / "static"
+#STATIC_ROOT = "/dsn_django/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
