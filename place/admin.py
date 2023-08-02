33fb2c4eaf081e4c183fd92b6e8b57f773aea97a
magusch <magusch@gmail.com>
2023-08-02 14:39:04 +0200

    version 0.2.5.1 - make keyword from place name

--- place/admin.py
+++ place/admin.py
@@ -4,6 +4,11 @@ from .models import Place, PlaceKeyword, TestEventPlace
 class PlaceAdmin(admin.ModelAdmin):
     list_display = [str,'place_name', 'place_url', 'url_to_address']
 
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        PlaceKeyword.objects.create(place_id=obj.id, place_keyword=obj.place_name)
+
+
 admin.site.register(Place, PlaceAdmin)
 admin.site.register(PlaceKeyword)
 admin.site.register(TestEventPlace)
