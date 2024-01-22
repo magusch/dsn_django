0593c8e6161daf3ea7e8702d8e16df0a52db3ae8
magusch <magusch@gmail.com>
2024-01-22 21:44:23 +0500

    version 0.2.7.9 – post_checker add check place is not empty

--- events/helper/post_checker.py
+++ events/helper/post_checker.py
@@ -14,6 +14,7 @@ class PostChecker:
         self.underscore()
         self.length()
         self.title()
+        self.place_empty()
 
     def asterisk(self):
         asterisk_len = len(re.findall(r"\*", self.post))  # how many asterisk in the post
@@ -41,3 +42,8 @@ class PostChecker:
         elif asterisk_len_title % 2 != 0:
             error_message = f"The title has odd number ({asterisk_len_title}) of * ."
             self.result['T'] = error_message
+
+    def place_empty(self):
+        if self.place is None:
+            error_message = f"The place field is empty."
+            self.result['P'] = error_message
