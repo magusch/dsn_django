1f73701d63aa4aa28c57738b5815900677bab64a
magusch <magusch@gmail.com>
2024-04-23 18:19:42 +0700

    version 0.2.10.0 - Class for ParameterManager with cash and small new function in post_helper

--- events/helper/load_parameters.py
+++ events/helper/load_parameters.py
@@ -1,10 +1,37 @@
+from django.core.cache import cache
 from ..models import Parameter
 
 
-def footer_link():
-    prms = Parameter.objects.filter(site='dsn_site').filter(parameter_name='finish_link')
-    footer_link_value = ''
-    if prms:
-        footer_link_value = prms[0].value
+class ParametersManager:
+    CACHE_KEY = 'parameters'
+    LOCK_KEY = 'parameters_lock'
 
-    return footer_link_value
+    def __new__(cls):
+        if not hasattr(cls, '_instance'):
+            if not hasattr(cls, '_instance'):
+                cls._instance = super().__new__(cls)
+                cls._instance.load_parameters_from_cache()
+        return cls._instance
+
+    def load_parameters_from_cache(self):
+        self._parameters = cache.get(self.CACHE_KEY)
+        if not self._parameters:
+            self.load_parameters_from_db()
+
+    def load_parameters_from_db(self):
+        parameters = Parameter.objects.filter(site='dsn_site').all()
+        self._parameters = {param.parameter_name: param.value for param in parameters}
+        self.save_parameters_to_cache()
+
+    def save_parameters_to_cache(self):
+        cache.set(self.CACHE_KEY, self._parameters, timeout=43200)
+
+    def get_parameter(self, parameter_name):
+        self.load_parameters_from_cache()
+        return self._parameters.get(parameter_name)
+
+    def update_parameters_from_db(self):
+        self.load_parameters_from_db()
+
+    def periodic_update(self):
+        self.update_parameters_from_db()
